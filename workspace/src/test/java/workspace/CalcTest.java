90ef224237f4663e6b47157c4a7a53482129ab68
92AsadullinSN01 <92AsadullinSN01@D92ASN-STEND.rcr92.cbr.ru>
2021-04-13 13:28:48 +0300

    More negative tests
    replace exceptions with -1

--- workspace/src/test/java/workspace/CalcTest.java
+++ workspace/src/test/java/workspace/CalcTest.java
@@ -2,6 +2,7 @@ package workspace;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CalcTest {
@@ -15,6 +16,33 @@ class CalcTest {
 	void testSumOnePlusTwo() {
 		Calc calc = new Calc();
 		assertEquals(3, calc.sum("1,2"));
+	}
+	
+	@Test
+	void testSumNull() {
+		Calc calc = new Calc();
+		assertEquals(-1, calc.sum(null));
+	}
+	
+	@Test
+	@DisplayName("сложение одного пустого аргумента")
+	void testSumEmpty() {
+		Calc calc = new Calc();
+		assertEquals(-1, calc.sum(""));
+	}
+	
+	@Test
+	@DisplayName("сложение одного аргумента")
+	void testSumOne() {
+		Calc calc = new Calc();
+		assertEquals(-1, calc.sum("1"));
+	}
+	
+	@Test
+	@DisplayName("сложение с рациональным")
+	void testSumRational() {
+		Calc calc = new Calc();
+		assertEquals(-1, calc.sum("0,0.1"));
 	}
 	
 	@Test
@@ -33,6 +61,35 @@ class CalcTest {
 	void testSumMinusOnePlusZero() {
 		Calc calc = new Calc();
 		assertEquals(-1, calc.sum("-1,0"));
+	}
+	
+	@Test
+	@DisplayName("Три аргумента с ошибкой текст в третьем аргументе")
+	void testSumOnePlusTwoPlusString() {
+		Calc calc = new Calc();
+		assertThrows(NumberFormatException.class, ()->calc.sum("1,2,еуые"));
+	}
+	
+	@Test
+	@DisplayName("Три аргумента с переполнением инт")
+	void testSumIntMaxPlusTwoPlusZero() {
+		Calc calc = new Calc();
+		assertEquals(2147483649L, calc.sum("2147483647,2,0"));
+	}
+	
+	@Test
+	@DisplayName("Три аргумента с ошибкой разделителя")
+	void testSumZeroPlusZeroPlusThree() {
+		Calc calc = new Calc();
+		assertEquals(-1, calc.sum("0,0ж3"));
+//		assertThrows(NumberFormatException.class, ()->calc.sum("0,0ж3"));
+	}
+	
+	@Test
+	@DisplayName("Три аргумента с негативным слагаемым")
+	void testSumMinusOnePlusZeroPlusTwo() {
+		Calc calc = new Calc();
+		assertEquals(1, calc.sum("-1,0,2"));
 	}
 	
 	@Test
@@ -57,5 +114,26 @@ class CalcTest {
 	void testSumMinusOnePlusMinusIntMax() {
 		Calc calc = new Calc();
 		assertEquals(-2147483648, calc.sum("-1,-2147483647"));
+	}
+	
+	@Test
+	void testSumStringPlusString() {
+		Calc calc = new Calc();
+//		assertThrows(ArithmeticException.class,()->{ calc.sum("test,test");});
+		assertEquals(-1, calc.sum("test,test"));
+	}
+	
+	@Test
+	void testSumStringPlusMaxInt() {
+		Calc calc = new Calc();
+//		assertThrows(ArithmeticException.class,()->{ calc.sum("str,2147483647");});
+		assertEquals(-1,calc.sum("str,2147483647"));
+	}
+	
+	@Test
+	void testSumMaxIntPlusChar() {
+		Calc calc = new Calc();
+		assertEquals(-1,calc.sum("-1,a"));
+//		assertThrows(ArithmeticException.class,()->{ calc.sum("-1,a");});
 	}
 }
