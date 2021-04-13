d0b6e2d47159095ce4aff65ca582b44a4dcc3d83
92AsadullinSN01 <92AsadullinSN01@D92ASN-STEND.rcr92.cbr.ru>
2021-04-13 13:04:26 +0300

    Added more test to sum

--- workspace/src/test/java/workspace/CalcTest.java
+++ workspace/src/test/java/workspace/CalcTest.java
@@ -12,8 +12,50 @@ class CalcTest {
 	}
 
 	@Test
-	void testSum() {
+	void testSumOnePlusTwo() {
 		Calc calc = new Calc();
-		assertEquals(3, calc.sum("1+2"));
+		assertEquals(3, calc.sum("1,2"));
+	}
+	
+	@Test
+	void testSumIntMaxPlusTwo() {
+		Calc calc = new Calc();
+		assertEquals(2147483649L, calc.sum("2147483647,2"));
+	}
+	
+	@Test
+	void testSumZeroPlusZero() {
+		Calc calc = new Calc();
+		assertEquals(0, calc.sum("0,0"));
+	}
+	
+	@Test
+	void testSumMinusOnePlusZero() {
+		Calc calc = new Calc();
+		assertEquals(-1, calc.sum("-1,0"));
+	}
+	
+	@Test
+	void testSumMinusOnePlusMinusZero() {
+		Calc calc = new Calc();
+		assertEquals(-1, calc.sum("-1,-0"));
+	}
+	
+	@Test
+	void testSumMinusOnePlusPositive10() {
+		Calc calc = new Calc();
+		assertEquals(9, calc.sum("-1,10"));
+	}
+	
+	@Test
+	void testSumMinusOnePlusPositiveIntMax() {
+		Calc calc = new Calc();
+		assertEquals(2147483646, calc.sum("-1,2147483647"));
+	}
+	
+	@Test
+	void testSumMinusOnePlusMinusIntMax() {
+		Calc calc = new Calc();
+		assertEquals(-2147483648, calc.sum("-1,-2147483647"));
 	}
 }
